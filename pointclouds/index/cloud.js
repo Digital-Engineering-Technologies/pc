612c053297afdf33c3df5f7dc07954d383e047ec
Oscar Jacobsen <Oscar.Jacobsen@norconsult.com>
2024-05-22 16:29:52 +0200

    132

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 14677511,
+    "boundingBox": {
+        "lx": 103872.841,
+        "ly": 1211400.29,
+        "lz": -12.568,
+        "ux": 104154.24,
+        "uy": 1211681.689,
+        "uz": 268.8310000000049
+    },
+    "tightBoundingBox": {
+        "lx": 103872.841,
+        "ly": 1211400.29,
+        "lz": -12.568,
+        "ux": 104154.24,
+        "uy": 1211564.916,
+        "uz": 61.128
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 2.4369869232177736,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
